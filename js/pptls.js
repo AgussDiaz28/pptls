f72b5417a489049186c84e5d0ca0e166b9cff09b
AgussDiaz28 <agussdiaz28@gmail.com>
2017-06-11 13:11:54 -0300

    Agregada nuevas funcionalidad de all in y Torneo

--- js/pptls.js
+++ js/pptls.js
@@ -10,8 +10,9 @@ $(document).ready(function() {
 
   PGanadas=0;
   Credito = 100;
+  PTotales = 0;
 
-  function random() {
+  function random() {                     //Me devuelve el valor de la maquina
     let objeto;
     let element = Math.floor((Math.random() * 4) + 1);  //round redondea los valores del random que va desde 0 a 2
 
@@ -27,21 +28,23 @@ $(document).ready(function() {
       objeto = "spock";
     }
     return objeto;
+    console.log(objeto);
   }
 
-  $("button").on("click",function () {                       //si se toco un boton, traeme el id del boton que se toco
+  $("button").on("click",function () {       //Me devuelve el ID del boton clikeado
          clicked = $(this).attr("id");
+         console.log('Se cliqueo: '+clicked);                              //BORRAR LOG
          comprar(clicked);
        });
 
-
-  function comprar(clicked) {                                               //MEJORAR, SE REPITE MUCHO EL CODIGO Y VERIFICAR FUNCIONAMIENTO
+  function comparar(clicked) {               //MEJORAR, SE REPITE MUCHO EL CODIGO Y VERIFICAR FUNCIONAMIENTO
     objetoC = random();
+    console.log('En el random salio: ' +objetoC);
             if (clicked == "piedra") {
-                if (jQuery.inArray(objetoC, gpiedra)){
+                if ( (jQuery.inArray(objetoC, gpiedra)) === 0 ){
                     $("#Resultado").html("GANO");
-                  PGanadas = PGanadas+1;
-                  credito += credito + ( $("#apuesta").val() * 2 );
+                    PGanadas = PGanadas+1;
+                    Credito = Credito + ( parseInt( $("#apuesta").val() * 2 ) );
                 }else if (objetoC == this) {
                   $("#Resultado").html("EMPATO");
                 }else{
@@ -49,67 +52,71 @@ $(document).ready(function() {
                 }
             }
             if (clicked === "papel") {
-              if (jQuery.inArray(objetoC, gpapel)){
-                  $("#Resultado").html("GANO");
-                PGanadas = PGanadas+1;
-              }else if (objetoC == this) {
-                $("#Resultado").html("EMPATO");
-              }else{
-                $("#Resultado").html("PERDIO");
+                if (jQuery.inArray(objetoC, gpapel)){
+                    $("#Resultado").html("GANO");
+                  PGanadas = PGanadas+1;
+                  Credito = Credito + ( parseInt( $("#apuesta").val() * 2 ) );
+                }else if (objetoC == this) {
+                  $("#Resultado").html("EMPATO");
+                }else{
+                  $("#Resultado").html("PERDIO");
             }}
             if (clicked === "tijera") {
-              if (jQuery.inArray(objetoC, gtijera)){
-                  $("#Resultado").html("GANO");
-                PGanadas = PGanadas+1;
-              }else if (objetoC == this) {
-                $("#Resultado").html("EMPATO");
-              }else{
-                $("#Resultado").html("PERDIO")
+                  if (jQuery.inArray(objetoC, gtijera)){
+                      $("#Resultado").html("GANO");
+                    PGanadas = PGanadas+1;
+                    Credito = Credito + ( parseInt( $("#apuesta").val() * 2 ) );
+                  }else if (objetoC == this) {
+                    $("#Resultado").html("EMPATO");
+                  }else{
+                    $("#Resultado").html("PERDIO")
             }}
             if (clicked === "lagarto") {
-              if (jQuery.inArray(objetoC, glagarto)){
-                  $("#Resultado").html("GANO");
-                PGanadas = PGanadas+1;
-              }else if (objetoC == this) {
-                $("#Resultado").html("EMPATO");
-              }else{
-                $("#Resultado").html("PERDIO");
-            }}
+                  if (jQuery.inArray(objetoC, glagarto)){
+                      $("#Resultado").html("GANO");
+                      PGanadas = PGanadas+1;
+                      Credito = Credito + ( parseInt( $("#apuesta").val() * 2 ) );
+                  }else if (objetoC == this) {
+                    $("#Resultado").html("EMPATO");
+                  }else{
+                    $("#Resultado").html("PERDIO");
+                }}
             if (clicked === "spock") {
-              if (jQuery.inArray(objetoC, gspock)){
-                $("#Resultado").html("GANO");
-                PGanadas = PGanadas+1;
-              }else if (objetoC == this) {
-                $("#Resultado").html("EMPATO");
-              }else{
-                $("#Resultado").html("PERDIO");
-            }}
+                  if (jQuery.inArray(objetoC, gspock)){
+                    $("#Resultado").html("GANO");
+                    PGanadas = PGanadas+1;
+                    Credito = Credito + ( parseInt( $("#apuesta").val() * 2 ) );
+                  }else if (objetoC == this) {
+                    $("#Resultado").html("EMPATO");
+                  }else{
+                    $("#Resultado").html("PERDIO");
+                }}
 
-            $("#PGanadas").html('Partidas Ganadas:'+PGanadas);
-
+            PTotales = PTotales + 1;
+            $("#PGanadas").html('Partidas Ganadas: ' + PGanadas);
+            $("#Monedas").html('Monedas Restantes: ' + Credito);
+            $("#PTotales").html('Total Partidas Jugadas: ' + PTotales);
     };
 
+  $("#AllIn").on("click",function () {
+    $("#apuesta").val(Credito);
+  });
 
-
-  $("#piedra").on("click",function () {
-
+  $("#myCheck").on("click",function () {
+      if ($("#myCheck").prop('checked',true)){
+        rondas = $("TorneoRondas").val();
+        EmpezarTorneo(rondas);
+      }
   })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  function EmpezarTorneo(rondas) {
+      for (var i = 0; i < rondas; i++) {
+        $("button").on("click",function () {       //Me devuelve el ID del boton clikeado
+               clicked = $(this).attr("id");
+             });
+        comparar(clicked);
+      }
+  }
 
 
 });
